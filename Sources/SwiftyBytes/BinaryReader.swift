8377fc3658ab43d24a76e74d6e7b5b43be0c8282
Xenoxiluna <xenoxiluna@live.com>
2020-05-06 21:10:16 -0500

    Update to public

--- Sources/SwiftyBytes/BinaryReader.swift
+++ Sources/SwiftyBytes/BinaryReader.swift
@@ -10,116 +10,116 @@ import Foundation
 
 public class BinaryReader {
     public private(set) var readIndex: Int
-    let data: BinaryReadableData
+    public let data: BinaryReadableData
   
     public init(_ data: BinaryReadableData, readIndex: Int = 0) {
         self.data = data
         self.readIndex = readIndex
     }
   
-    func readUInt8() throws -> UInt8 {
+    public func readUInt8() throws -> UInt8 {
         let value: UInt8 = try data.getUInt8(readIndex)
         readIndex = readIndex + MemoryLayout<UInt8>.size
         return value
     }
   
-    func readInt8() throws -> Int8 {
+    public func readInt8() throws -> Int8 {
         let value: Int8 = try data.getInt8(readIndex)
         readIndex = readIndex + MemoryLayout<Int8>.size
         return value
     }
   
-    func readUInt16() throws -> UInt16 {
+    public func readUInt16() throws -> UInt16 {
         let value: UInt16 = try data.getUInt16(readIndex)
         readIndex = readIndex + MemoryLayout<UInt16>.size
         return value
     }
   
-    func readInt16() throws -> Int16 {
+    public func readInt16() throws -> Int16 {
         let value: Int16 = try data.getInt16(readIndex)
         readIndex = readIndex + MemoryLayout<Int16>.size
         return value
     }
   
-    func readUInt32() throws -> UInt32 {
+    public func readUInt32() throws -> UInt32 {
         let value: UInt32 = try data.getUInt32(readIndex)
         readIndex = readIndex + MemoryLayout<UInt32>.size
         return value
     }
 
-    func readInt32() throws -> Int32 {
+    public func readInt32() throws -> Int32 {
         let value: Int32 = try data.getInt32(readIndex)
         readIndex = readIndex + MemoryLayout<Int32>.size
         return value
     }
 
-    func readUInt64() throws -> UInt64 {
+    public func readUInt64() throws -> UInt64 {
         let value: UInt64 = try data.getUInt64(readIndex)
         readIndex = readIndex + MemoryLayout<UInt64>.size
         return value
     }
 
-    func readInt64() throws -> Int64 {
+    public func readInt64() throws -> Int64 {
         let value: Int64 = try data.getInt64(readIndex)
         readIndex = readIndex + MemoryLayout<Int64>.size
         return value
     }
 
-    func readFloat32() throws -> Float32 {
+    public func readFloat32() throws -> Float32 {
         let value: Float32 = try data.getFloat32(readIndex)
         readIndex = readIndex + MemoryLayout<Float32>.size
         return value
     }
 
-    func readFloat64() throws -> Float64 {
+    public func readFloat64() throws -> Float64 {
         let value: Float64 = try data.getFloat64(readIndex)
         readIndex = readIndex + MemoryLayout<Float64>.size
         return value
     }
     
-    func readFloat() throws -> Float {
+    public func readFloat() throws -> Float {
         let value: Float = try data.getFloat(readIndex)
         readIndex = readIndex + MemoryLayout<Float>.size
         return value
     }
     
-    func readDouble() throws -> Double {
+    public func readDouble() throws -> Double {
         let value: Double = try data.getDouble(readIndex)
         readIndex = readIndex + MemoryLayout<Double>.size
         return value
     }
 
-    func readNullTerminatedString() throws -> String {
+    public func readNullTerminatedString() throws -> String {
         let string = try data.getNullTerminatedString(readIndex)
         readIndex = readIndex + string.utf8.count + 1//Add 1 for \0
         return string
     }
     
-    open func readNullTerminatedStringNoTrail() throws -> String {
+    public func readNullTerminatedStringNoTrail() throws -> String {
       let string = try data.getNullTerminatedString(readIndex)
       readIndex = readIndex + string.utf8.count
       return string
     }
     
-    open func readStringEncoded(_ data: BinaryReadableData) throws -> String {
+    public func readStringEncoded(_ data: BinaryReadableData) throws -> String {
       let string = try data.getStringEncoded(readIndex)
       readIndex = readIndex + string.utf8.count
       return string
     }
 
-    func readString(_ length: Int) throws -> String {
+    public func readString(_ length: Int) throws -> String {
         let string = try data.getString(readIndex, length: length)
         readIndex = readIndex + length
         return string
     }
     
-    func read(_ length: Int) throws -> BinaryReadableData {
+    public func read(_ length: Int) throws -> BinaryReadableData {
         let subdata: BinaryReadableData = try data.subData(readIndex, length)
         readIndex = readIndex + length
         return subdata
     }
     
-    func read(_ length: Int) throws -> [UInt8] {
+    public func read(_ length: Int) throws -> [UInt8] {
         let subdata: [UInt8] = try data.subData(readIndex, length)
         readIndex = readIndex + length
         return subdata
--- Sources/SwiftyBytes/BinaryWriter.swift
+++ Sources/SwiftyBytes/BinaryWriter.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public class BinaryWriteer {
     public var writeIndex: Int
-    var data: BinaryWritableData
+    public var data: BinaryWritableData
   
     public init(_ data: BinaryWritableData, writeIndex: Int = 0) {
         self.data = data
@@ -22,73 +22,73 @@ public class BinaryWriteer {
         self.writeIndex = 0
     }
     
-    func writeUInt8(_ uint: UInt8) throws -> Bool {
+    public func writeUInt8(_ uint: UInt8) throws -> Bool {
         let value: Bool = try data.writeUInt8(uint)
         writeIndex = writeIndex + MemoryLayout<UInt8>.size
         return value
     }
   
-    func writeInt8(_ int: Int8) throws -> Bool {
+    public func writeInt8(_ int: Int8) throws -> Bool {
         let value: Bool = try data.writeInt8(int)
         writeIndex = writeIndex + MemoryLayout<Int8>.size
         return value
     }
   
-    func writeUInt16(_ uint: UInt16) throws -> Bool {
+    public func writeUInt16(_ uint: UInt16) throws -> Bool {
         let value: Bool = try data.writeUInt16(uint)
         writeIndex = writeIndex + MemoryLayout<UInt16>.size
         return value
     }
   
-    func writeInt16(_ int: Int16) throws -> Bool {
+    public func writeInt16(_ int: Int16) throws -> Bool {
         let value: Bool = try data.writeInt16(int)
         writeIndex = writeIndex + MemoryLayout<Int16>.size
         return value
     }
   
-    func writeUInt32(_ uint: UInt32) throws -> Bool {
+    public func writeUInt32(_ uint: UInt32) throws -> Bool {
         let value: Bool = try data.writeUInt32(uint)
         writeIndex = writeIndex + MemoryLayout<UInt32>.size
         return value
     }
 
-    func writeInt32(_ int: Int32) throws -> Bool {
+    public func writeInt32(_ int: Int32) throws -> Bool {
         let value: Bool = try data.writeInt32(int)
         writeIndex = writeIndex + MemoryLayout<Int32>.size
         return value
     }
 
-    func writeUInt64(_ uint: UInt64) throws -> Bool {
+    public func writeUInt64(_ uint: UInt64) throws -> Bool {
         let value: Bool = try data.writeUInt64(uint)
         writeIndex = writeIndex + MemoryLayout<UInt64>.size
         return value
     }
 
-    func writeInt64(_ int: Int64) throws -> Bool {
+    public func writeInt64(_ int: Int64) throws -> Bool {
         let value: Bool = try data.writeInt64(int)
         writeIndex = writeIndex + MemoryLayout<Int64>.size
         return value
     }
 
-    func writeFloat32(_ float: Float32) throws -> Bool {
+    public func writeFloat32(_ float: Float32) throws -> Bool {
         let value: Bool = try data.writeFloat32(float)
         writeIndex = writeIndex + MemoryLayout<Float32>.size
         return value
     }
 
-    func writeFloat64(_ float: Float64) throws -> Bool {
+    public func writeFloat64(_ float: Float64) throws -> Bool {
         let value: Bool = try data.writeFloat64(float)
         writeIndex = writeIndex + MemoryLayout<Float64>.size
         return value
     }
 
-    func writeString(_ string: String, encoding: String.Encoding) throws -> Bool {
+    public func writeString(_ string: String, encoding: String.Encoding) throws -> Bool {
         let value = try data.writeString(string, encoding: encoding)
         writeIndex = writeIndex + string.utf8.count + 1//Account for \0
         return value
     }
 
-    func write(_ bytes: [UInt8]) throws -> Bool {
+    public func write(_ bytes: [UInt8]) throws -> Bool {
         let value = try data.write(bytes)
         writeIndex = writeIndex + bytes.count
         return value
